6d60921d5b5efac1352ee8b43fd8654b4d359466
Jovi <nean@protonmail.com>
2020-11-07 14:48:06 -0300

    Challenge done withouth bonus.

--- 1-Beginner/Bin2Dec/bin2dec.cpp
+++ 1-Beginner/Bin2Dec/bin2dec.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 int convert2Dec(int bin){
-	return 0;
+	int number = 0;
+	for(int i = 7;i > -1; i--){
+			double power = pow(10,i);
+			if((bin - power) > 0){
+				number = number + pow(2, i);
+				bin = bin - power;
+			}else if((bin - power) == 0){
+				number++;
+			}
+	}
+	return number;
 }
 
 
 int main(){
+	int number;
+	cin >> number;
+	cout << convert2Dec(number) << endl;
 	return 0;
 }
